7a3758bd92fdca80a676d4d673feb3be19bf8689
Rakshith2597 <rakshith.sathish@gmail.com>
2020-10-26 01:00:23 +0530

    server added

--- miriadvtt/settings.py
+++ miriadvtt/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.9.7.52','127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['10.9.7.26','127.0.0.1','0.0.0.0']
 
 
 # Application definition
