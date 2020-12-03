280dcf2a2f4649b7b20ec1defc16f994ec46f16f
Rakshith2597 <rakshith.sathish@gmail.com>
2020-12-03 16:20:02 +0530

    Initial webpage of wp3 added

--- vttapp/static/js/custom_script.js
+++ vttapp/static/js/custom_script.js
@@ -32,10 +32,10 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
+var span = document.getElementsByClassName("close")[1];
 
 // When the user clicks on <span> (x), close the modal
-span[1].onclick = function() { 
+span.onclick = function() { 
   modal.style.display = "none";
 }
 
@@ -53,7 +53,50 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
+var span = document.getElementsByClassName("close")[2];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+var modal = document.getElementById("myModal4");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg4");
+var modalImg = document.getElementById("img04");
+// var captionText = document.getElementById("caption3");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  // captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[3];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+var modal = document.getElementById("myModal5");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg5");
+var modalImg = document.getElementById("img05");
+// var captionText = document.getElementById("caption3");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  // captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[4];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
