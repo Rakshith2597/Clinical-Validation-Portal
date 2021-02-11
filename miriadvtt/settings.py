ba4187aec880641428bacab3be1117af3ad5ff84
Rakshith2597 <rakshith.sathish@gmail.com>
2021-02-11 12:31:48 +0530

    v0.1 with wp3

--- miriadvtt/settings.py
+++ miriadvtt/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['10.9.7.26','127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['10.9.7.26','127.0.0.1','0.0.0.0','projectmiriadkliv.tk','projectmiriadkliv']
 
 
 # Application definition
